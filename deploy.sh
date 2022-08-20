0609cce727aae4304640e7285c0b6af545d145cf
Alaa.ELHamouly <alaa.elhamouly@andalusiagroup.net>
2022-08-20 12:49:28 +0200

    preparing for deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -en
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f git@github.com:Alaahamouly/customer-crud.git master:gh-pages
+
+cd -
